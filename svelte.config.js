4133d77b359bda40bef8667885c4cd27aabbbadf
Krish Gupta <m.krishggupta@icloud.com>
2025-10-06 23:40:55 +0530

    chore: use vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,14 @@
-import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-vercel";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx']
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: [vitePreprocess(), mdsvex()],
+  kit: { adapter: adapter() },
+  extensions: [".svelte", ".svx"],
 };
 
 export default config;
